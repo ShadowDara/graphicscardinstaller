436b8a61b3ab6a9055bc560ba35b15b486392bd1
Shadowdara <128976697+ShadowDara@users.noreply.github.com>
2025-05-20 17:05:44 +0200

    page is now!

--- page/src/routes/docs/+page.svelte
+++ page/src/routes/docs/+page.svelte
@@ -4,7 +4,7 @@
 	<div class="flex flex-1 flex-col items-center justify-center text-xl/10">
 		<h1 class="text-6xl/20">Docs</h1>
         <h2><i>Graphic Card Installer</i></h2>
-		<img src="fan_quad.svg" alt="Fan quad graphic">
+		<img src="/fan_quad.svg" alt="Fan quad graphic">
     </div>
 </main>
 
--- page/svelte.config.js
+++ page/svelte.config.js
@@ -1,23 +1,24 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.svx'],
-
-  preprocess: [
-    mdsvex({ extensions: ['.svx'] })
-  ],
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({
-      // Option für statischen Export
-      pages: 'build', // Ausgabeordner für die statischen Seiten
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
+      strict: true,
       paths: {
-        base: '/graphicscardinstaller'
-      },
-      assets: 'build', // Wo die Assets (Bilder, CSS, JS) abgelegt werden
-      fallback: null, // Keine SPA-Index.html, sondern jede Route als eigene HTML
-      strict: true, // Überprüft, dass alle Seiten statisch sind
+        base: '/graphicscardinstaller/'
+      }
     })
   }
 };
