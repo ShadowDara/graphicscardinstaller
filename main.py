b46bc6021ccb0a5d5dfc03a1f0c7a0957d783ba4
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-08-19 13:11:30 +0200

    PUB

---
+++ main.py
@@ -0,0 +1,16 @@
+# Graphic Card Installer
+# by Shadowdara
+# Licensed under Apache License 2.0
+# https://github.com/ShadowDara/graphicscardinstaller
+
+# Better Debbuging
+from rich.traceback import install
+install()
+
+import pyapp.terminal_load as l
+
+def main():
+    l.main()
+
+if __name__ == "__main__":
+    main()
